5b742d693c9d76c6d0d603b7fe8e695d11a6592e
Riza May Largadas <rmlargadas@gmail.com>
2024-06-03 17:49:18 +0800

    browse, movies, tv_show, languages, filter by genre and language ok

--- movie_tv/urls.py
+++ movie_tv/urls.py
@@ -5,6 +5,12 @@ app_name = 'movie_tv'
 
 urlpatterns = [
     path('<int:profile_pk>/browse/', views.browse, name='browse'),
+    path('<int:profile_pk>/tv_show/', views.tv_show, name='tv_show'),
+    path('<int:profile_pk>/movies/', views.movies, name='movies'),
+    path('<int:profile_pk>/new_popular/',
+         views.new_popular, name='new_popular'),
+    path('<int:profile_pk>/browse_by_language/',
+         views.browse_by_language, name='browse_by_language'),
     path('<uuid:movie_pk>/watch/', views.watch, name="watch"),
     path('<int:profile_pk>/<uuid:movie_pk>/vote/', views.vote, name="vote"),
 ]
--- movie_tv/views.py
+++ movie_tv/views.py
@@ -3,8 +3,10 @@ from django.contrib.auth.decorators import login_required
 from profiles.models import Profile
 from movie_tv.models import Movie
 from django.urls import reverse
-from .models import Vote
+from .models import Vote, Genre, Language
 from django.db.models import Exists, OuterRef
+from django.utils.timezone import make_aware
+from datetime import datetime, timedelta
 
 
 def browse(request, profile_pk):
@@ -19,7 +21,7 @@ def browse(request, profile_pk):
     else:
         movies = Movie.objects.all()
 
-    featured_movie = movies.get(is_featured=True)
+    featured_movie = movies.get(is_featured=True, show_type='movie')
 
     context = {
         'profile': profile,
@@ -42,3 +44,135 @@ def vote(request, profile_pk, movie_pk):
         Vote.objects.create(profile=profile, movie=movie)
 
     return redirect(reverse('movie_tv:browse', args=[profile_pk]))
+
+
+def tv_show(request, profile_pk):
+    profile = get_object_or_404(Profile, pk=profile_pk)
+    profiles = Profile.objects.filter(account=profile.account)
+    genres = Genre.objects.all()
+    query = request.GET.get('q', '')
+    message = ''
+
+    if profile and query:
+        movies = Movie.objects.filter(show_type='tv_show').annotate(
+            has_voted=Exists(Vote.objects.filter(
+                profile=profile, movie=OuterRef('pk')))
+        )
+        movies = movies.filter(genre__name__icontains=query)
+        if not movies:
+            message = f"No TV show available for {query}."
+    else:
+        movies = Movie.objects.filter(show_type='tv_show')
+
+    featured_movie = get_object_or_404(
+        Movie, is_featured=True, show_type='tv_show')
+    context = {
+        'profile': profile,
+        'profiles': profiles,
+        'movies': movies,
+        'featured_movie': featured_movie,
+        'genres': genres,
+        'message': message,
+        'query': query
+    }
+    return render(request, 'movie_tv/tv_show.html', context)
+
+
+def movies(request, profile_pk):
+    profile = get_object_or_404(Profile, pk=profile_pk)
+    profiles = Profile.objects.filter(account=profile.account)
+    genres = Genre.objects.all()
+    query = request.GET.get('q', '')
+    message = ''
+
+    if profile and query:
+        movies = Movie.objects.filter(show_type='movie').annotate(
+            has_voted=Exists(Vote.objects.filter(
+                profile=profile, movie=OuterRef('pk')))
+        )
+        movies = movies.filter(genre__name__icontains=query)
+        if not movies:
+            message = f"No movie available for {query}."
+    else:
+        movies = Movie.objects.filter(show_type='movie')
+
+    featured_movie = get_object_or_404(
+        Movie, is_featured=True, show_type='movie')
+    context = {
+        'profile': profile,
+        'profiles': profiles,
+        'movies': movies,
+        'featured_movie': featured_movie,
+        'genres': genres,
+        'message': message,
+        'query': query
+    }
+    return render(request, 'movie_tv/movies.html', context)
+
+
+def new_popular(request, profile_pk):
+    profile = get_object_or_404(Profile, pk=profile_pk)
+    profiles = Profile.objects.filter(account=profile.account)
+    genres = Genre.objects.all()
+    query = request.GET.get('q', '')
+    message = ''
+
+    one_month_ago = make_aware(datetime.now() - timedelta(days=30))
+
+    if profile and query:
+        movies = Movie.objects.filter(show_type='movie').annotate(
+            has_voted=Exists(Vote.objects.filter(
+                profile=profile, movie=OuterRef('pk')))
+        )
+        movies = movies.filter(genre__name__icontains=query)
+        if not movies:
+            message = f"No movie available for {query}."
+    else:
+        movies = Movie.objects.filter(show_type='movie')
+
+    movies = movies.filter(date_added__gte=one_month_ago)
+
+    featured_movie = get_object_or_404(
+        Movie, is_featured=True, show_type='movie')
+    context = {
+        'profile': profile,
+        'profiles': profiles,
+        'movies': movies,
+        'featured_movie': featured_movie,
+        'genres': genres,
+        'message': message,
+        'query': query
+    }
+    return render(request, 'movie_tv/new_popular.html', context)
+
+
+def browse_by_language(request, profile_pk):
+    profile = get_object_or_404(Profile, pk=profile_pk)
+    profiles = Profile.objects.filter(account=profile.account)
+    languages = Language.objects.all()
+    query = request.GET.get('q', '')
+    message = ''
+
+    if profile and query:
+        movies = Movie.objects.annotate(
+            has_voted=Exists(Vote.objects.filter(
+                profile=profile, movie=OuterRef('pk')))
+        )
+        movies = movies.filter(language__name__icontains=query)
+        if not movies:
+            message = f"No movie/show available for {query}."
+    else:
+        movies = Movie.objects.all()
+
+    featured_movie = get_object_or_404(
+        Movie, is_featured=True, show_type='movie')
+    context = {
+        'profile': profile,
+        'profiles': profiles,
+        'movies': movies,
+        'featured_movie': featured_movie,
+        'languages': languages,
+        'message': message,
+        'query': query
+    }
+    return render(request, 'movie_tv/browse_by_language.html', context)
